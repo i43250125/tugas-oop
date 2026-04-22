b70538b86de92033af4bc93ee170f8d3d443ca66
BARCA <i43250125@student.polije.ac.id>
2026-04-22 19:13:27 +0700

    after

--- counter_pengunjung.php
+++ counter_pengunjung.php
@@ -7,12 +7,22 @@ class Pengunjung {
     public function __construct() {
         self::$jumlah++;
     }
+
+    public static function reset() {
+        self::$jumlah = 0;
+    }
 }
 
 $p1 = new Pengunjung();
 $p2 = new Pengunjung();
 $p3 = new Pengunjung();
+$p4 = new Pengunjung();
+$p5 = new Pengunjung();
 
-echo "Jumlah Pengunjung: " . Pengunjung::$jumlah;
+echo "Jumlah Pengunjung Sebelum Reset: " . Pengunjung::$jumlah . "\n";
+
+Pengunjung::reset();
+
+echo "Jumlah Pengunjung Sesudah Reset: " . Pengunjung::$jumlah . "\n";
 
 ?>
